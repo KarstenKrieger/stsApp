c9d4c921ba4d8f8315a93b8af7f0d31b962d9457
Karsten Krieger <office@comp-soft-media.de>
2025-02-08 11:46:56 +0100

    Dashboard  Nachrichten

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -3,7 +3,7 @@
         <div class="dashboard-default dashboard-main">
             <ejs-dashboardlayout ref="dashboard" :columns="5" id='defaultLayout' :allowResizing="true" :cellSpacing="spacing">
                 <e-panels>
-                    <e-panel></e-panel>
+                    <e-panel id="layout_0" :row="0" :col="0" :sizeX="1" :sizeY="1" header="<span id='close' class='e-template-icon e-clear-icon'> </span><div class='e-panel-header'></div>'" content="<div class='panel-content'>Content Area</div>"></e-panel>
                 </e-panels>
             </ejs-dashboardlayout>
         </div>
@@ -29,8 +29,8 @@
                 errorMsg: '',
                 spacing: [10, 10],
                 panels: [],
-                panelShow: 0,
                 contactData: {},
+                dashboardMessages: {},
                 salutation: '',
                 isLead: false,
                 consentDataProcessingText: ''
@@ -45,12 +45,17 @@
         mounted: async function () {
             if (!this.token)
                 router.push('login');
-            this.contactData = await this.getCustomerInformation();
-            if (!this.isLead) {
-                this.addPanel(0, 0, 0, 1, 1, '... Willkommen auf unserem Portal!', '<div style="padding: 10px; color:black;"><p>Herzlich Willkommen auf unserer STSApp, ' + this.salutation + '!</p><p>Ihrer zentralen Anlaufstelle für die Verwaltung Ihrer persönlichen, finanziellen und kommunikativen Daten. Unser Ziel ist es, Ihnen eine sichere und benutzerfreundliche Plattform zu bieten, auf der Sie jederzeit und von überall aus Ihre Informationen verwalten können.</p><h4>Was Sie hier tun können:</h4><ul><li><strong>Persönliche Daten verwalten</strong>: Halten Sie Ihre persönlichen Informationen stets aktuell.</li><li><strong>Finanzdaten erfassen und einsehen</strong>: Behalten Sie den Überblick über Ihre finanzbezogenen Informationen.</li><li><strong>Kommunikation optimieren</strong>: Nutzen Sie unsere sicheren Kanäle, um schnell und effizient zu kommunizieren.</li><li><strong>Dokumente hochladen und organisieren</strong>: Laden Sie wichtige Unterlagen sicher hoch und greifen Sie jederzeit darauf zu.</li></ul><h4>Ihre Sicherheit ist uns wichtig!</h4><p>Wir legen höchsten Wert auf Datenschutz und Datensicherheit. Ihre Informationen werden mit modernsten Technologien geschützt und nur für die Zwecke verwendet, denen Sie ' + this.consentDataProcessingText +' haben. Sie können die Zustimmung in Ihrem Profil ändern.</p><h4>Einfach. Sicher. Transparent.</h4><p>Melden Sie sich an und erleben Sie, wie einfach es ist, Ihre Daten und Dokumente an einem Ort zu verwalten. Sollten Sie Fragen haben, steht Ihnen unser Support-Team jederzeit zur Verfügung.</p><p>Wir freuen uns, Sie auf der STSApp begrüßen zu dürfen!</p></div>');
-            }
-            else
-                this.addPanel(0, 0, 0, 1, 1, 'Willkommen auf unserem Portal!', '<div style="padding: 10px; color:black;"><p>Herzlich Willkommen auf unserer STSApp, UNBEKANNT <br/>Ihrer zentralen Anlaufstelle für die Verwaltung Ihrer persönlichen, finanziellen und kommunikativen Daten. Unser Ziel ist es, Ihnen eine sichere und benutzerfreundliche Plattform zu bieten, auf der Sie jederzeit und von überall aus Ihre Informationen verwalten können.</p><h4>Was Sie hier tun können:</h4><ul><li><strong>Persönliche Daten verwalten</strong>: Halten Sie Ihre persönlichen Informationen stets aktuell.</li><li><strong>Finanzdaten erfassen und einsehen</strong>: Behalten Sie den Überblick über Ihre finanzbezogenen Informationen.</li><li><strong>Kommunikation optimieren</strong>: Nutzen Sie unsere sicheren Kanäle, um schnell und effizient zu kommunizieren.</li><li><strong>Dokumente hochladen und organisieren</strong>: Laden Sie wichtige Unterlagen sicher hoch und greifen Sie jederzeit darauf zu.</li></ul><h4>Ihre Sicherheit ist uns wichtig!</h4><p>Wir legen höchsten Wert auf Datenschutz und Datensicherheit. Ihre Informationen werden mit modernsten Technologien geschützt und nur für die Zwecke verwendet, denen Sie zugestimmt haben. Sie können die Zustimmung in Ihrem Profil ändern.</p><h4>Einfach. Sicher. Transparent.</h4><p>Melden Sie sich an und erleben Sie, wie einfach es ist, Ihre Daten und Dokumente an einem Ort zu verwalten. Sollten Sie Fragen haben, steht Ihnen unser Support-Team jederzeit zur Verfügung.</p><p>Wir freuen uns, Sie auf der STSApp begrüßen zu dürfen!</p></div>');
+            await this.getCustomerInformation();
+            await this.getDashboardMessages();
+            this.$refs.dashboard.removePanel('layout_0');
+            this.dashboardMessages.sort((a, b) => a.sortorder - b.sortorder)
+            this.dashboardMessages.forEach((m) => {
+                var row = m.sortorder % 5;
+                var col = Math.floor(m.sortorder / 5);
+                m.message = m.message.replace("##salutation##", this.salutation);
+                m.message = m.message.replace("##consentdataprocessingtext##", this.consentDataProcessingText);
+                this.addPanel(m.sortorder, col, row, 1, 1, m.title, '<div style="padding: 10px; color:black;">' + m.message + '</div>');
+            })
         },
 
         methods: {
@@ -65,14 +70,15 @@
                     content: content
                 }];
                 this.panels.push(panel);
-                this.panelShow++;
                 this.$refs.dashboard.$el.ej2_instances[0].addPanel(panel[0]);
+                this.panelShow++;
                 var closeIcon = document.getElementById('layout_' + pid.toString()).querySelector('.e-clear-icon');
                 closeIcon.addEventListener('click', this.onCloseIconHandler);
             },
             onCloseIconHandler: function (event) {
                 if (event.target.offsetParent) {
-                    this.$refs.dashboard.removePanel(event.target.offsetParent.id);
+                    if (event.target.offsetParent.id != 'layout_0')
+                        this.$refs.dashboard.removePanel(event.target.offsetParent.id);
                 }
                 this.panelShow--;
             },
@@ -101,7 +107,30 @@
                     .catch((err) => {
                         this.showError('Unterstützungsdienste sind nicht erreichbar oder login fehlerhaft. ' + err)
                     })
+            },
+            getDashboardMessages: async function () {
+                const requestOptions = {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin': '*',
+                        'content-type': 'application/json',
+                        'Accept': 'application/json',
+                        'Authorization': 'Bearer ' + this.token
+                    }
+                }
+
+                var selv = this;
+
+                await fetch(this.$serviceBaseUrl + 'Contact/GetDashboardMessages', requestOptions)
+                    .then(async resp => {
+                        selv.dashboardMessages = await resp.json()
+                    })
+                    .catch((err) => {
+                        this.showError('Unterstützungsdienste sind nicht erreichbar oder login fehlerhaft. ' + err)
+                    })
             }
+
         }
     }
 </script>
